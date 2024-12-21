77c3491de79ea85a918ed7cfedf9cf12de22db34
gjin <gjinrexhaj1@gmail.com>
2024-12-21 14:57:11 +0000

    update info

--- components/volunteering/volunteering-item.jsx
+++ components/volunteering/volunteering-item.jsx
@@ -1,22 +1,26 @@
 import React from "react";
+import Image from "next/image";
 
 function VolunteeringItem(props) {
     return (
-        <div className="group flex flex-row mb-4 p-5 transition-all bg-background hover:bg-surface-200">
-            <div className='mr-2 text-surface-600 text-xs basis-1/4'>{props.startDate} {checkEndDate(props)}</div>
-            <div className='basis-3/4 flex flex-col'>
-                <a href={props.href} target="_blank" rel="noopener noreferrer" className='font-medium transition-all'>{props.position} </a>
-                <a href={props.href} target="_blank" rel="noopener noreferrer" className='text-surface-600 mb-4 flex flex-row items-center'>
-                    <span>{props.organisation}</span>
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" class="w-3 h-3 ml-1 invisible transition ease-in-out scale-50 group-hover:visible group-hover:scale-100">
-                        <path strokeLinecap="round" strokeLinejoin="round" d="M4.5 19.5l15-15m0 0H8.25m11.25 0v11.25" />
-                    </svg>
-                </a>
-                <div className='text-surface-600'>{props.description}</div>
+        <div className="group flex flex-row mb-4 p-5 transition-all hover:bg-surface-200">
+            <div className="basis-1/4 mr-2">
+                <Image src={props.logo} alt="Company Logo" width={90} height={70} className='object-contain object-top pt-2' />
+            </div>
+            <div className='basis-3/4'>
+                <a href={props.href} target="_blank" rel="noopener noreferrer" className='font-medium transition-all'>{props.title} </a>
+                <div className='mb-2 text-surface-600'>{props.startDate} - {props.endDate}</div>
+                <div className='text-surface-600 mb-4'>{props.description}</div>
+                <div className='flex flex-row'>
+                    {props.skills ? props.skills.map(function(object, index){
+                        return <div key={object} className='bg-surface-400 py-1 px-3 rounded-full text-xs mr-2'>{object}</div>
+                    }) : ""}
+                </div>
             </div>
         </div>
     )
 }
+
 
 function checkEndDate(props) {
     if (!props.endDate) {
--- components/volunteering/volunteering.jsx
+++ components/volunteering/volunteering.jsx
@@ -7,10 +7,9 @@ const Volunteering = (props) => {
             <h2 className='mb-8 visible lg:invisible font-medium tracking-widest'>Volunteering</h2>
             {props.data.map(function(object, index){
                 return <VolunteeringItem  
-                    key={`${object.position}+${object.startDate}`}
-                    position={object.position}
-                    event={object.event} 
-                    organisation={object.organisation}
+                    key={`${object.title}+${object.startDate}`}
+                    title={object.title}
+                    company={object.company}
                     href={object.href}
                     startDate={object.startDate}
                     endDate={object.endDate ? object.endDate : ''}
@@ -20,5 +19,33 @@ const Volunteering = (props) => {
         </div>
     )
 }
+
+/*
+import React from "react";
+import ExperienceItem from "./experience-item";
+
+const Experiences = (props) => {
+    return (
+        <div data-section id='experiences' className='mb-16'>
+            <h2 className='mb-8 visible lg:invisible font-medium tracking-widest'>Experiences</h2>
+            {props.data.map(function(object, index){
+                return <ExperienceItem  
+                    key={`${object.title}+${object.startDate}`}
+                    title={object.title}
+                    company={object.company}
+                    href={object.href}
+                    startDate={object.startDate}
+                    endDate={object.endDate}
+                    description={object.description}
+                    logo={object.logo}
+                    skills={object.skills}
+                />
+            })}
+        </div>
+    )
+}
+
+export default Experiences
+*/
 
 export default Volunteering
