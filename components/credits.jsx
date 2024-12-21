9595c38dfd9ee079e20ebd7758761ba9a834efa8
gjin <gjinrexhaj1@gmail.com>
2024-12-21 15:48:13 +0000

    Finishing touches

--- components/credits.jsx
+++ components/credits.jsx
@@ -5,10 +5,10 @@ const Credits = (props) => {
         <div data-section id='credits' className='group mt-32'>
             <div className='text-surface-600'>
                 <div className='mb-4'>
-                    This website was built using a <a href="https://github.com/karlgrossmann/two-column-portfolio" target="_blank" rel="noopener noreferrer" className="text-on-background transition-all hover:text-primary-500">template</a> by <a href="https://karlgrossmann.com" target="_blank" rel="noopener noreferrer" className="text-on-background transition-all hover:text-primary-500">Karl Grossmann</a> using <a href="https://nextjs.org" target="_blank" rel="noopener noreferrer" className="text-on-background transition-all hover:text-primary-500">Next.js</a> and 
-                    <a href="https://tailwindcss.com" target="_blank" rel="noopener noreferrer" className="text-on-background transition-all hover:text-primary-500"> Tailwind CSS</a>. 
-                    The inspiration for the design comes from <a href="https://www.sarahdayan.dev" target="_blank" rel="noopener noreferrer" className="text-on-background transition-all hover:text-primary-500">Sarah Dayan </a> 
-                    and <a href="https://brittanychiang.com" target="_blank" rel="noopener noreferrer" className="text-on-background transition-all hover:text-primary-500">Brittany Chiang</a>. 
+                    <a>Email is the quickest way to get ahold of me, feel free to reach out with any inquiries!</a>
+                    <br></br> <br></br>
+                    This website was built using <a href="https://nextjs.org" target="_blank" rel="noopener noreferrer" className="text-on-background transition-all hover:text-primary-500">Next.js</a> and 
+                    <a href="https://tailwindcss.com" target="_blank" rel="noopener noreferrer" className="text-on-background transition-all hover:text-primary-500"> Tailwind CSS.</a>
                 </div>
             </div>
         </div>
